1ad5710d5faf41d67a50d44fe1c0c9dc5f14e237
AngadOnTop <angadjotd@gmail.com>
2025-06-11 13:26:35 +1000

    optimisation

--- src/main.js
+++ src/main.js
@@ -9,69 +9,74 @@ kaplay({
 })
 
 setGravity(1600)
-let score = 0
-const IMAGE_WIDTH = 1280
 
-// UI - Score Text at top middle
+const WIDTH = 1280
+const HEIGHT = 720
+const SCORE_FONT = 24
+const GROUND_TILE_COUNT = 20
+const SPEED = 300
+let score = 0
+let blob = null
+
+// Load assets
+await Promise.all([
+  loadSprite("smiley", "/sprites/evenBetterSheet.png", {
+    sliceX: 13,
+    sliceY: 1,
+    anims: {
+      idle: { from: 0, to: 3, speed: 8, loop: true },
+      jump: { from: 4, to: 7, speed: 8, loop: false },
+      right: { from: 11, to: 11 },
+      left: { from: 12, to: 12 },
+    },
+  }),
+  loadSprite("spike", "/sprites/smallSpike.png"),
+  loadSprite("longSpike", "/sprites/longSpike.png"),
+  loadSprite("coin", "/sprites/coin.png"),
+  loadSprite("ground", "/sprites/ground.png"),
+  loadSprite("dirt", "/sprites/dirt.png"),
+  loadSprite("background", "/sprites/background.jpg"),
+
+  loadSound("jump", "/sounds/jump.wav"),
+  loadSound("collectingCoin", "/sounds/coin.wav"),
+  loadSound("hit", "/sounds/hit.wav"),
+  loadSound("death", "/sounds/death.wav"),
+])
+
+// Background
+add([
+  sprite("background"),
+  pos(0, 0),
+  scale(Math.max(width() / WIDTH, height() / HEIGHT)),
+  anchor("topleft"),
+  fixed(),
+  z(-999),
+])
+
+// Score UI
 const scoreLabel = add([
-  text(`Score: ${score}`, { size: 24 }),
-  pos(640, 16),
+  text(`Score: ${score}`, { size: SCORE_FONT }),
+  pos(WIDTH / 2, 16),
   anchor("center"),
   fixed(),
   z(100),
   color(0, 0, 0),
 ])
 
-// Sprites
-await loadSprite("smiley", "/sprites/evenBetterSheet.png", {
-  sliceX: 13,
-  sliceY: 1,
-  anims: {
-    idle: { from: 0, to: 3, speed: 8, loop: true },
-    jump: { from: 4, to: 7, speed: 8, loop: false },
-    right: { from: 11, to: 11 },
-    left: { from: 12, to: 12 },
-  },
-})
-await loadSprite("spike", "/sprites/smallSpike.png")
-await loadSprite("longSpike", "/sprites/longSpike.png")
-await loadSprite("coin", "/sprites/coin.png")
-await loadSprite("ground", "/sprites/ground.png")
-await loadSprite("background", "/sprites/background.jpg")
-await loadSprite("dirt", "/sprites/dirt.png")
-
-await loadSound("jump", "/sounds/jump.wav")
-await loadSound("collectingCoin", "/sounds/coin.wav")
-await loadSound("hit", "/sounds/hit.wav")
-await loadSound("death", "/sounds/death.wav")
-
-// Background image
-add([
-  sprite("background"),
-  pos(0, 0),
-  scale(Math.max(width() / 1280, height() / 720)),
-  anchor("topleft"),
-  fixed(),
-  z(-999),
-])
-
-// Ground + Dirt Layers
-for (let i = 0; i < 20; i++) {
+// Ground + Dirt
+for (let i = 0; i < GROUND_TILE_COUNT; i++) {
   const x = 200 + i * 64
   const y = 672
 
-  // Top ground tile
   add([
     sprite("ground"),
     scale(4),
-    outline(10),
     pos(x, y),
     area(),
     body({ isStatic: true }),
   ])
 
-  // Dirt layers below the ground
-  for (let j = 1; j <= 6; j++) {
+  for (let j = 1; j <= 3; j++) {
     add([
       sprite("dirt"),
       scale(4),
@@ -90,27 +95,26 @@ add([
   body({ isStatic: true }),
 ])
 
-// Enemies
+// Spikes
 add([
   sprite("spike"),
   area(),
   body({ isStatic: true }),
   pos(400, 640),
-  outline(2),
-  "smallSpike",
   scale(4),
+  "smallSpike",
 ])
 
 add([
   sprite("longSpike"),
   area(),
   body({ isStatic: true }),
-  outline(2),
-  "longSpike",
-  scale(4),
   pos(450, 612),
+  scale(4),
+  "longSpike",
 ])
 
+// Coin logic
 function spawnCoin() {
   const y = 624
   const coin = add([
@@ -123,15 +127,11 @@ function spawnCoin() {
 
   let goingUp = true
 
-  coin.onUpdate(() => {
+  loop(1.0, () => {
+    if (!coin.exists()) return
     const offset = goingUp ? -15 : 15
-    tween(
-      coin.pos.y,
-      coin.pos.y + offset,
-      0.8,
-      (val) => coin.pos.y = val,
-      easings.linear
-    ).then(() => goingUp = !goingUp)
+    tween(coin.pos.y, coin.pos.y + offset, 0.8, (val) => coin.pos.y = val)
+    goingUp = !goingUp
   })
 
   return coin
@@ -139,9 +139,7 @@ function spawnCoin() {
 
 let coin = spawnCoin()
 
-let blob
-const SPEED = 300
-
+// Player logic
 function spawnBlob() {
   blob = add([
     sprite("smiley", { anim: "idle" }),
@@ -150,22 +148,20 @@ function spawnBlob() {
     area(),
     body(),
     health(100, 100),
-    "player",
     rotate(),
+    "player",
   ])
 
   const hpBarBg = blob.add([
     rect(40, 6),
     color(255, 0, 0),
     pos(-14, -20),
-    z(1),
   ])
 
   const hpBar = blob.add([
     rect(40, 6),
     color(0, 255, 0),
     pos(-14, -20),
-    z(2),
   ])
 
   const hpText = blob.add([
@@ -173,25 +169,12 @@ function spawnBlob() {
     color(0, 0, 0),
     pos(5, -16),
     anchor("center"),
-    z(3),
   ])
 
   blob.onUpdate(() => {
     const ratio = blob.hp() / blob.maxHP()
     hpBar.width = 40 * ratio
     hpText.text = `${Math.max(blob.hp(), 0)}/${blob.maxHP()}`
-  })
-
-  onKeyPress(["space", "w"], () => {
-    if (blob && blob.isGrounded()) {
-      blob.jump()
-      blob.play("jump")
-      play("jump")
-    }
-  })
-
-  blob.onAnimEnd(() => {
-    blob.play("idle")
   })
 
   blob.onCollide("smallSpike", () => {
@@ -206,21 +189,32 @@ function spawnBlob() {
 
   blob.onCollide("coin", (c) => {
     destroy(c)
-    score += 1
+    score++
     scoreLabel.text = `Score: ${score}`
     play("collectingCoin")
     coin = spawnCoin()
   })
 
+  blob.onAnimEnd(() => blob.play("idle"))
+
   blob.onDeath(() => {
     destroy(blob)
-    play("death")
     blob = null
+    play("death")
+  })
+
+  onKeyPress(["space", "w"], () => {
+    if (blob && blob.isGrounded()) {
+      blob.jump()
+      blob.play("jump")
+      play("jump")
+    }
   })
 }
 
 spawnBlob()
 
+// Controls
 onKeyDown("d", () => {
   if (blob) {
     blob.play("right")
@@ -235,6 +229,18 @@ onKeyDown("a", () => {
   }
 })
 
+// Respawn button
+add([
+  text("Respawn", { size: 32 }),
+  pos(WIDTH / 2, 300),
+  anchor("center"),
+  area(),
+  color(0, 0, 0),
+]).onClick(() => {
+  if (!blob) spawnBlob()
+})
+
+// Camera follow + offscreen detection
 onUpdate(() => {
   if (!blob) return
 
@@ -246,18 +252,4 @@ onUpdate(() => {
   }
 
   setCamPos(blob.pos)
-})
-
-const respawnBtn = add([
-  text("Respawn", { size: 32 }),
-  pos(640, 300),
-  anchor("center"),
-  area(),
-  color(0, 0, 0),
-])
-
-respawnBtn.onClick(() => {
-  if (!blob) {
-    spawnBlob()
-  }
 })
